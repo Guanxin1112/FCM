c88f51da2290f8105ef0f41896b5d11a2236da7f
Guanxin1112 <71569831+Guanxin1112@users.noreply.github.com>
2024-06-20 15:50:09 +0800

    Sim_prediction.R

--- Simulation/Sim_prediction.R
+++ Simulation/Sim_prediction.R
@@ -9,18 +9,18 @@ library(fda)
 filepathfun = '~/Functions/'
 source(paste(filepathfun,'three_step.R',sep=''))
 source(paste(filepathfun,'prediction.R',sep=''))
-source(paste(filepathfun,'datasim',sep=''))
+source(paste(filepathfun,'datasim.R',sep=''))
 
 
 # sample size
 n0 = 20; m = 600
-nrep = 200
-q = 3
-
 n = m+n0
+q = 3
 k = floor(n^(1/(2*q+1))); k1 = floor(n^(1/(2*q+1)))
 h1 = c(0.3*(2000/n)^(-1/5),0.2*(2000/n)^(-1/5));
 h2 = c(0.12*(2000/n)^(-1/5),0.12*(2000/n)^(-1/5))
+
+nrep = 200
 MSPE = rep(NA, nrep)
   
 for(i in 1:nrep){
@@ -33,4 +33,4 @@ for(i in 1:nrep){
   MSPE[i] = nonpara$MSPE_ker
 }
   
-  
+mean(MSPE)
